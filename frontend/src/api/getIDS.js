da66f3414b2492a67be47dfbd71c95c6de883644
Ivan Sharapov <oldrum75@gmail.com>
2024-02-29 16:53:33 +0100

    fix: test env

--- frontend/src/api/getIDS.js
+++ frontend/src/api/getIDS.js
@@ -10,7 +10,8 @@ const createAuthString = (password) => {
 };
 
 const getIDS = async (params) => {
-  const password = process.env.REACT_APP_VALANTIS_PASS || 'Valantis';
+  // const password = process.env.REACT_APP_VALANTIS_PASS || 'Valantis';
+  const password = 'Valantis';
   const url = 'http://api.valantis.store:40000/';
   const authString = createAuthString(password);
 
