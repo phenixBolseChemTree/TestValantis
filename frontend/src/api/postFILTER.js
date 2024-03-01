c279ef148cd23524a2cc2d3906e64271c42cfea5
Ivan Sharapov <oldrum75@gmail.com>
2024-03-01 16:58:37 +0100

    test: postFIELD

---
+++ frontend/src/api/postFILTER.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
+import md5 from 'js-md5';
+
+const createAuthString = (password) => {
+  const date = new Date();
+  const timestamp = `${date.getUTCFullYear()}${(date.getUTCMonth() + 1).toString().padStart(2, '0')}${date.getUTCDate().toString().padStart(2, '0')}`;
+  return md5(`${password}_${timestamp}`);
+};
+
+const postIDS = async (params) => {
+  // const password = process.env.REACT_APP_VALANTIS_PASS || 'Valantis';
+  const password = 'Valantis';
+  const url = 'http://api.valantis.store:40000/';
+  const authString = createAuthString(password);
+
+  const data = {
+    action: 'get_ids',
+    params
+  };
+
+  // Включение механизма повтора 5 раз
+  axiosRetry(axios, { retries: 5 });
+
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        'X-Auth': authString
+      }
+    });
+    return response.data;
+  } catch (error) {
+    // console.log('postIDS: 500');
+
+    // console.log('Retrying request IDS...');
+
+    return postIDS(params);
+  }
+};
+
+export default postIDS;
--- frontend/src/components/searchSortWidget/SearchSortWidget.jsx
+++ frontend/src/components/searchSortWidget/SearchSortWidget.jsx
@@ -1,7 +1,20 @@
 import styles from './searchSortWidget.module.css';
+import TextField from '@mui/material/TextField';
 
 const SearchSortWidget = () => {
-  return <div className={styles.root}>SearchSortWidget123</div>;
+  return (
+    <div className={styles.root}>
+      <form style={{ width: '85%' }} noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="Введите текст"
+          variant="outlined"
+          fullWidth
+          // Дополнительные стили можно добавить здесь
+        />
+      </form>
+    </div>
+  );
 };
 
 export default SearchSortWidget;
